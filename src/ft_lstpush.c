e403c232c76d665c034447c2666d16fffc39ced4
Visentin <tvisenti@42.student.fr>
2016-05-18 16:47:45 +0200

    Ne push pas le bon nombre

--- src/ft_lstpush.c
+++ src/ft_lstpush.c
@@ -6,7 +6,7 @@
 /*   By: tvisenti <tvisenti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/18 13:33:16 by tvisenti          #+#    #+#             */
-/*   Updated: 2016/05/18 16:22:48 by tvisenti         ###   ########.fr       */
+/*   Updated: 2016/05/18 16:46:42 by tvisenti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,13 @@ void	ft_push_a(t_list *lst_a, t_list *lst_b)
 	if (lst_a->head != NULL)
 	{
 		elem = lst_a->head;
-		if (lst_b->tail)
+		if (lst_b)
 		{
-			printf("val %d\n", elem->val);
 			lst_a->head = elem->next;
 			elem->next = NULL;
 			lst_b->head = elem;
 			lst_b->tail = elem;
-			printf("val %d\n", elem->val);
 		}
-		// else
 		// {
 		// 	new_head = lst_b->head;
 		// 	lst_b->head = elem;
