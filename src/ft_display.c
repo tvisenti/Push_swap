3bd05e13f14b271f65cfb89deca212949e9aabcb
Visentin <tvisenti@42.student.fr>
2016-05-19 11:31:16 +0200

    Display + clair

--- src/ft_display.c
+++ src/ft_display.c
@@ -6,7 +6,7 @@
 /*   By: tvisenti <tvisenti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/19 08:25:41 by tvisenti          #+#    #+#             */
-/*   Updated: 2016/05/19 11:25:54 by tvisenti         ###   ########.fr       */
+/*   Updated: 2016/05/19 11:30:54 by tvisenti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,48 +15,49 @@
 void	ft_manage_display(t_list *lst_a, t_list *lst_b)
 {
 	printf("======== FIRST DISPLAY=============\n");
-	ft_display_a(lst_a);
-	ft_display_b(lst_b);
+	ft_display(lst_a, lst_b);
 
 	printf("========== 1x SWAP A ==============\n");
 	ft_swap_a(lst_a);
-	ft_display_a(lst_a);
-	ft_display_b(lst_b);
+	ft_display(lst_a, lst_b);
 
-	printf("=========== 3x PUSH B ===========\n");
+	printf("============ 3x PUSH B ============\n");
 	ft_push_b(lst_a, lst_b);
 	ft_push_b(lst_a, lst_b);
 	ft_push_b(lst_a, lst_b);
-	ft_display_a(lst_a);
-	ft_display_b(lst_b);
+	ft_display(lst_a, lst_b);
 
-	printf("=========== 1x ROTATE RR ===========\n");
+	printf("========== 1x ROTATE RR ===========\n");
 	ft_rotate_rr(lst_a, lst_b);
-	ft_display_a(lst_a);
-	ft_display_b(lst_b);
+	ft_display(lst_a, lst_b);
 
-	printf("=========== 1x REV ROTATE RRR ===========\n");
+	printf("======== 1x REV ROTATE RRR ========\n");
 	ft_rev_rotate_rrr(lst_a, lst_b);
-	ft_display_a(lst_a);
-	ft_display_b(lst_b);
+	ft_display(lst_a, lst_b);
 
 	printf("========== 1x SWAP A ==============\n");
 	ft_swap_a(lst_a);
-	ft_display_a(lst_a);
-	ft_display_b(lst_b);
+	ft_display(lst_a, lst_b);
 
-	printf("=========== 1x PUSH B ===========\n");
+	printf("============ 1x PUSH B ============\n");
 	ft_push_a(lst_a, lst_b);
 	ft_push_a(lst_a, lst_b);
 	ft_push_a(lst_a, lst_b);
-	ft_display_a(lst_a);
-	ft_display_b(lst_b);
+	ft_display(lst_a, lst_b);
 }
 
 void	ft_error()
 {
 	write(2, "Error\n", 6);
 	exit(1);
+}
+
+void	ft_display(t_list *lst_a, t_list *lst_b)
+{
+	printf("\n");
+	printf("\t"); ft_display_a(lst_a);
+	printf("\t"); ft_display_b(lst_b);
+	printf("\n");
 }
 
 void	ft_display_a(t_list *lst_a)
